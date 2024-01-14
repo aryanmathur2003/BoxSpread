a4326b8f4989bcd22a32b55c2edaeb5c538f6b12
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2024-01-13 21:48:42 -0500

    fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5%fdm!hg#!^w9o9=b-afff2c0okdr206%j9#x#39g-q&#p9)0!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
